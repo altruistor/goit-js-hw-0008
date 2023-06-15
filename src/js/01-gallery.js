61839fc5a41b04daa5ec8384aaaa95149c57e31c
altruistor <36813847+altruistor@users.noreply.github.com>
2023-06-15 21:21:46 +0200

    123456

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,29 +6,9 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-import { galleryItems } from './gallery-items.js';
-// import * as basicLightbox from 'basiclightbox'
-// Change code below this line
-
-
-
-
-
-
-
-
 const gallery = document.querySelector('.gallery');
 
-const imagesMarkup = createImageMarkup(galleryItems);
-
-gallery.insertAdjacentHTML('beforeend', imagesMarkup);
-
-
-
-
-function createImageMarkup(galleryItems) {
-    return galleryItems
-    .map(({preview, original, description}) => {
+const createImageMarkup = galleryItems.map(({preview, original, description}) => {
         return `
         <li class="gallery__item">
         <a class="gallery__link" href="${original}">
@@ -44,23 +24,18 @@ function createImageMarkup(galleryItems) {
     .join('');
 
 
-}
 
-function openImage(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
+
+gallery.insertAdjacentHTML('beforeend', createImageMarkup);
+
+
     
     
   const instance = new SimpleLightbox('.gallery a', { 
-    
-    
-
+    captionPosition: "bottom",
+    captionData: "alt",
+    captionDelay: 250,
      });
 
-    instance.show();
-}
 
 
-gallery.addEventListener('click', openImage);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,112 @@
 
-import throttle from "lodash";
+// import throttle from 'lodash/throttle';
 
-const keyStorage = "feedback-form-state"
+// const keyStorage = "feedback-form-state"
+// const feedbackForm = document.querySelector('.feedback-form');
+// const emailInput = document.querySelector("input")
+// const messageInput = document.querySelector("textarea");
+// const submitButton = document.querySelector("button");
+
+
+
+
+// feedbackForm.addEventListener('submit', evt => {
+//     evt.preventDefault();
+
+//     console.log(emailInput.value);
+
+//     const markup = {
+//         email: emailInput.value,
+//         message: messageInput.value,
+//     }
+//     localStorage.setItem(keyStorage, JSON.stringify(markup));
+
+//     feedbackForm.elements.email.value = '';
+//     feedbackForm.elements.message.value = '';
+
+    
+   
+//     console.log(markup);
+    
+// });
+
+
+
+// const updateLocalStorage = throttle(() => {
+//   const markup = {
+//     email: emailInput.value,
+//     message: messageInput.value,
+//   };
+//   localStorage.setItem(keyStorage, JSON.stringify(markup));
+// }, 500);
+
+// emailInput.addEventListener(emailInput, updateLocalStorage);
+// messageInput.addEventListener(messageInput, updateLocalStorage);
+
+// feedbackForm.addEventListener('submit', throttle(evt => {
+//     evt.preventDefault();
+
+//     console.log(emailInput.value);
+
+//     const markup = {
+//         email: emailInput.value,
+//         message: messageInput.value,
+//     }
+//     localStorage.setItem(keyStorage, JSON.stringify(markup));
+
+//     feedbackForm.elements.email.value = '';
+//     feedbackForm.elements.message.value = '';
+
+//     console.log(markup);
+// }, 500));
+
+// window.addEventListener('load', () => {
+//   const savedState = localStorage.getItem(keyStorage);
+//   if (savedState) {
+//     const markup = JSON.parse(savedState);
+//     emailInput.value = markup.email;
+//     messageInput.value = markup.message;
+//   }
+// });
+
+// const valuesObject = () => {
+//     const markup = {
+//         email: feedBackForm.elements.email.value,
+//         message: feedBackForm.elements.textarea.value,
+//     }
+//     localStorage.setItem(keyStorage, JSON.stringify(markup));
+// } 
+
+// feedBackForm.addEventListener("input", valuesObject)
+
+// console.log(valuesObject);
+
+
+
+  
+
+
+
+
+import throttle from "lodash/throttle";
+
+const keyStorage = "feedback-form-state";
 const feedbackForm = document.querySelector('.feedback-form');
-const emailInput = document.querySelector("input")
+const emailInput = document.querySelector("input");
 const messageInput = document.querySelector("textarea");
-const submitButton = document.querySelector("button");
 
 
+const updateLocalStorage = throttle(() => {
+  const markup = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(keyStorage, JSON.stringify(markup));
+}, 500);
 
+
+emailInput.addEventListener('input', updateLocalStorage);
+messageInput.addEventListener('input', updateLocalStorage);
 
 feedbackForm.addEventListener('submit', evt => {
     evt.preventDefault();
@@ -29,28 +127,3 @@ feedbackForm.addEventListener('submit', evt => {
     console.log(markup);
     
 });
-
-window.addEventListener('load', () => {
-  const savedState = localStorage.getItem(keyStorage);
-  if (savedState) {
-    const markup = JSON.parse(savedState);
-    emailInput.value = markup.email;
-    messageInput.value = markup.message;
-  }
-});
-
-// const valuesObject = () => {
-//     const markup = {
-//         email: feedBackForm.elements.email.value,
-//         message: feedBackForm.elements.textarea.value,
-//     }
-//     localStorage.setItem(keyStorage, JSON.stringify(markup));
-// } 
-
-// feedBackForm.addEventListener("input", valuesObject)
-
-// console.log(valuesObject);
-
-
-
-  
